c20b965a0f40fe15f2dc43a03a166e68d44615b9
G3rrus <daniel_schreiner@live.at>
2022-05-26 10:50:04 +0200

    Add i18n

--- app/main.client.jsx
+++ app/main.client.jsx
@@ -3,6 +3,7 @@ import { Meteor } from 'meteor/meteor';
 import { render } from 'react-dom';
 import { Routes } from './ui/common/Routes';
 import { Spinner } from '@chakra-ui/react';
+import i18n from 'meteor/universe:i18n';
 
 /**
  * This is the client-side entry point
@@ -10,6 +11,12 @@ import { Spinner } from '@chakra-ui/react';
 Meteor.startup(() => {
   document.documentElement.setAttribute('lang', 'en');
   const rootElement = document.getElementById('react-target');
+  i18n.setOptions({  // <--- 6
+    defaultLocale: 'de-DE'
+  });
+  // eslint-disable-next-line no-console
+  i18n.setLocale('de-DE');
+  console.log(i18n.getLocale());
   render(
     <Suspense fallback={<Spinner />}>
       <Routes />
